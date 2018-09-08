48241aa64c147912b82dcce995fb324b973513be
Ernane Luis <ernane.luis@gmail.com>
2018-09-09 00:42:30 +0800

    refactor ipfs lib

--- src/app/lib/ipfs.js
+++ src/app/lib/ipfs.js
@@ -10,21 +10,13 @@ export const init = () => {
 
 export const store = ({ data }) => {
   // data is buffer
-  node.files.add(data, (err, res) => {
-    if (err || !res) return console.error('ipfs add error', err, res)
-
-    res.forEach(file => {
-      if (file && file.hash) console.log('successfully stored', file.hash)
-    })
+  return node.files.add({
+    path: 'myfileEncrypted.txt',
+    content: data,
   })
 }
 
 export const get = ({ hash }) => {
   // buffer: true results in the returned result being a buffer rather than a stream
-  node.files.cat(hash, (err, data) => {
-    if (err) return console.error('ipfs cat error', err)
-    console.log('get data ', data.toString())
-    // document.getElementById('hash').innerText = hash
-    // document.getElementById('content').innerText = data
-  })
+  return node.files.cat(hash)
 }
