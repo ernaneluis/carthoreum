b6e20ca0b70d87c4e1ba633af912afaf4539c46f
Ernane Luis <ernane.luis@gmail.com>
2018-09-18 00:34:13 +0800

    add test case to sign the document and reject if the document cant be signed

--- ethereum/test/Documents.test.js
+++ ethereum/test/Documents.test.js
@@ -14,18 +14,20 @@ function getLastEvent(instance) {
 
 const msg = '7e5941f066b2070419995072dac7323c02d5ae107b23d8085772f232487fecae'
 const ipfsHash = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'
+const allowedSigners = [
+  '0x39df5973289461639367e1dbaaf32715d14d6a49',
+  '0xa1d59c562c72500b21da64f2244f809c3749c1c4',
+]
 
 contract('Documents', function(accounts) {
   it('should create a Document and reject a duplicate Document and retrieve data from document', async function() {
-    const account = accounts[0]
-
     const instance = await Documents.deployed()
     const sha3Hash = web3.utils.sha3(msg)
 
-    await instance.addDocument(sha3Hash, ipfsHash)
+    await instance.createDocument(allowedSigners, sha3Hash, ipfsHash)
     const eventObj = await getLastEvent(instance)
-    assert.equal(eventObj.event, 'AddDocument')
-    console.log('AddDocument event')
+    console.log('event ', eventObj.event)
+    assert.equal(eventObj.event, 'CreateDocument')
 
     const doesDocumentExists = await instance.doesDocumentExists(sha3Hash)
     assert.equal(doesDocumentExists, true)
@@ -35,7 +37,7 @@ contract('Documents', function(accounts) {
     assert.equal(docFromContract[2], ipfsHash)
 
     try {
-      await instance.addDocument(sha3Hash, ipfsHash)
+      await instance.createDocument(allowedSigners, sha3Hash, ipfsHash)
     } catch (error) {
       assert.ok(true)
     }
@@ -45,17 +47,41 @@ contract('Documents', function(accounts) {
     const instance = await Documents.deployed()
     const sha3Hash = web3.utils.sha3(msg + 1)
 
-    await instance.addDocument(sha3Hash, ipfsHash)
+    await instance.createDocument(
+      [...allowedSigners, accounts[0]],
+      sha3Hash,
+      ipfsHash
+    )
 
-    const doesDocumentExists = await instance.doesDocumentExists(sha3Hash)
-    assert.equal(doesDocumentExists, true)
+    await instance.signDocument(sha3Hash)
+
+    const eventObj = await getLastEvent(instance)
+    console.log('event ', { eventObj })
+    assert.equal(eventObj.event, 'DidSignDocument')
 
     const didSign = await instance.didSignDocument(sha3Hash)
-    console.log({ didSign })
     assert.equal(didSign, true)
 
     const didNotSign = await instance.didSignDocument('notSign')
-    console.log({ didNotSign })
     assert.equal(didNotSign, false)
+  })
+
+  it('create a Document which cant sign', async function() {
+    const instance = await Documents.deployed()
+    const sha3Hash = web3.utils.sha3(msg + 2)
+    console.log({ sha3Hash })
+    await instance.createDocument(allowedSigners, sha3Hash, ipfsHash)
+
+    try {
+      await instance.signDocument(sha3Hash)
+    } catch (error) {
+      assert.ok(true)
+    }
+
+    const didSign = await instance.didSignDocument(sha3Hash)
+    assert.equal(didSign, false)
+
+    const docFromContract = await instance.getDocument(sha3Hash)
+    console.log({ docFromContract })
   })
 })
