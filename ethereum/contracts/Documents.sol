fad1cb1970965f3808a780f079cf02a4ebf41c15
Ernane Luis <ernane.luis@gmail.com>
2018-09-18 00:33:47 +0800

    add multiple signers to document. add count of documents add DidSignDocument event add canSignDocument modifier refactor signDocument function by adding safety check canSignDocument modifier

--- ethereum/contracts/Documents.sol
+++ ethereum/contracts/Documents.sol
@@ -3,40 +3,55 @@ pragma solidity ^0.4.24;
 import "./Ownable.sol";
 
 contract Documents is Ownable {
+
+    event DebugBool(bool varBool);
+
+    /*
+     *  Constants
+    */
+    uint constant public MAX_ALLOWED_SIGNERS_COUNT = 10;
+
     // TODO add all the missing data to struct
     struct Document {
         uint timestamp;
         bytes32 sha3Hash;
         string ipfsHash;
         address[] allowedSigners;
-        address[] signeders;
-        mapping(address => bool) signatures;
+        address[] signatures;
+        mapping(address => bool) canSign;
+        mapping(address => bool) didSign;
     }
-
-    event AddDocument(uint timestamp, bytes32 indexed sha3Hash, string ipfsHash,  address[] allowedSigners, address[] signeders);
-    // TODO create sign event
 
     mapping(bytes32 => Document) public documents;
+    uint public totalDocuments;
 
-    // TODO safety check max of signers are 10
-    function addDocument(bytes32 sha3Hash,  string ipfsHash) public notExistsDocument(sha3Hash) {    
-        address[] memory allowedSigners = new address[](1);
-        allowedSigners[0] = msg.sender;
-        // TODO add signers address as input
-        address[] memory signeders = new address[](1);
-        signeders[0] = msg.sender;
 
-        documents[sha3Hash] = Document(now, sha3Hash, ipfsHash, allowedSigners, signeders);
-        documents[sha3Hash].signatures[msg.sender] = true;
-
-        emit AddDocument(now, sha3Hash, ipfsHash, allowedSigners, signeders);
-    }
+    event CreateDocument(uint timestamp, bytes32 indexed sha3Hash, string ipfsHash, address[] allowedSigners);
+    event DidSignDocument(uint timestamp, bytes32 indexed sha3Hash, address signer);
     
-    // TODO only allowed signers can sign
-    function signDocument(bytes32 sha3Hash) public {
-        // TODO check if sender can sign document
-        documents[sha3Hash].signatures[msg.sender] = true;
-        documents[sha3Hash].signeders.push(msg.sender);
+
+    
+
+    function createDocument(address[] _allowedSigners, bytes32 sha3Hash,  string ipfsHash) public notExistsDocument(sha3Hash) {    
+        require(_allowedSigners.length <= MAX_ALLOWED_SIGNERS_COUNT, "Maximum 10 signers");
+
+        address[] memory signatures = new address[](1);
+       
+        documents[sha3Hash] = Document(now, sha3Hash, ipfsHash, _allowedSigners, signatures);
+
+        for (uint i = 0; i < _allowedSigners.length; i++) {
+            require(!documents[sha3Hash].canSign[_allowedSigners[i]] && _allowedSigners[i] != 0, "Allowed Signers cannt be empty/null");
+            documents[sha3Hash].canSign[_allowedSigners[i]] = true;
+        }
+
+        emit CreateDocument(now, sha3Hash, ipfsHash, _allowedSigners);
+        totalDocuments++;
+    }
+
+    function signDocument(bytes32 sha3Hash) public canSignDocument(sha3Hash) {
+        documents[sha3Hash].didSign[msg.sender] = true;
+        documents[sha3Hash].signatures.push(msg.sender);
+        emit DidSignDocument(now, sha3Hash, msg.sender);
     }
 
     function doesDocumentExists(bytes32 sha3Hash) public view returns (bool) {
@@ -44,23 +59,27 @@ contract Documents is Ownable {
         return documents[sha3Hash].sha3Hash != bytes32(0);
     }
 
-    modifier notExistsDocument(bytes32 sha3Hash) {
-        require(!doesDocumentExists(sha3Hash),  "Document already have been added!");
-        _;
-    }
 
     function getDocument(bytes32 sha3Hash) public view returns (uint,bytes32,string,address[],address[]) {
         return (documents[sha3Hash].timestamp,
         documents[sha3Hash].sha3Hash,
         documents[sha3Hash].ipfsHash,
         documents[sha3Hash].allowedSigners,
-        documents[sha3Hash].signeders);
+        documents[sha3Hash].signatures);
     }
 
 
     function didSignDocument(bytes32 sha3Hash) public view returns (bool) {
-        return documents[sha3Hash].signatures[msg.sender] == true;
+        return documents[sha3Hash].didSign[msg.sender] == true;
     }
 
-    // TODO number of documents added
+    modifier notExistsDocument(bytes32 sha3Hash) {
+        require(!doesDocumentExists(sha3Hash),  "Document already have been added!");
+        _;
+    }
+
+    modifier canSignDocument(bytes32 sha3Hash) {
+        require(documents[sha3Hash].canSign[msg.sender],  "Address is not allowed to sign this document");
+        _;
+    }
 }
