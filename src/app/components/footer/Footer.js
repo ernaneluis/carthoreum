b2ab28b9f03a06b05e95a26f27fa9b74ec5cdb98
Ernane Luis <ernane.luis@gmail.com>
2018-09-20 13:11:29 +0800

    bugfix footer on top of page content when page gets bigger

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -4,11 +4,9 @@ import styled from 'styled-components'
 const FooterWrapper = styled.footer.attrs({
   className: 'footer',
 })`
-  position: absolute;
-  bottom: 0;
   width: 100%;
-  height: 60px;
-  line-height: 60px;
+  height: 100px;
+  line-height: 100px;
   background-color: #f5f5f5;
 `
 
