ddfd15e536d95184b9c30422a8224f2ef779dc47
Ernane Luis <ernane.luis@gmail.com>
2018-09-21 02:12:50 +0800

    refactor encrypt function to be event emitter to support progress stream

--- src/app/lib/encryption.js
+++ src/app/lib/encryption.js
@@ -2,6 +2,8 @@ import Wallet from 'ethereumjs-wallet'
 import ecies from 'eth-ecies'
 import * as openpgp from 'openpgp'
 
+const EventEmitter = require('events').EventEmitter
+
 openpgp.initWorker({ path: './openpgp.worker.js' }) // set the relative web worker path
 
 export const createSharedKey = () => Wallet.generate().getPrivateKey()
@@ -12,16 +14,40 @@ export const encrypt = ({ data, ethereumPublicKey }) =>
 export const decrypt = ({ encryptedData, ethereumPrivateKey }) =>
   ecies.decrypt(ethereumPrivateKey, encryptedData)
 
-export const encryptFile = async ({ data, password }) => {
-  console.log('encryptFile')
+export const encryptFile = ({ data, password }) => {
+  let result = []
+  let emitter = new EventEmitter()
+
+  const readableStream = new ReadableStream({
+    start(controller) {
+      controller.enqueue(data)
+      controller.close()
+    },
+  })
+
   const options = {
-    message: await openpgp.message.fromBinary(data), // input as Message object
+    message: openpgp.message.fromBinary(readableStream), // input as Message object
     passwords: [password], // multiple passwords possible
     armor: false, // don't ASCII armor (for Uint8Array output)
   }
-  return openpgp.encrypt(options).then(
-    ciphertext => ciphertext.message.packets.write() // get raw encrypted packets as Uint8Array
-  )
+
+  openpgp.encrypt(options).then(async ciphertext => {
+    // get raw encrypted packets as Uint8Array
+    const encrypted = ciphertext.message.packets.write()
+    const reader = openpgp.stream.getReader(encrypted)
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) {
+        emitter.emit('done', new Uint8Array(result))
+        return new Uint8Array(result)
+      } else {
+        emitter.emit('encrypting', value)
+        // value for fetch streams is a Uint8Array
+        result = [...result, ...value]
+      }
+    }
+  })
+  return emitter
 }
 
 export const decryptFile = async ({ encryptedData, password }) => {
